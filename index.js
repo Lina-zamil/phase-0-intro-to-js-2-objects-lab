963d255811ba37a84d53d3d09c1fb38f02163abb
lina abumahfood <lina.abumahfood@gmail.com>
2023-09-29 16:31:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+const employee = { name: "lina", streetAddress: "jordan" };
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+// function updateEmployeeWithKeyAndValue(obj, key) {
+//     delete obj[key]=value;
+
+// };
+function deleteFromEmployeeByKey(obj, key) {
+    const newObjj = { ...obj };
+    delete newObjj[key];
+    return newObjj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+};
+
+
+
